907f37598d2e239e8ed4f0f1a1504b9e09ff048a
wallef <wallefsousa1@gmail.com>
2019-03-11 19:46:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Botões Social Media</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">	
+</head>
+<body>
+
+	<div class="midias">
+		
+		<a class="btn" href="#">
+
+			<i class="fab fa-facebook-f"></i>
+		</a>
+		<a class="btn"  href="#">
+
+			<i class="fab fa-instagram"></i>
+		</a>
+
+		<a class="btn"  href="#">
+
+			<i class="fab fa-google"></i>
+		</a>
+
+		<a class="btn"  href="#">
+
+			<i class="fab fa-youtube"></i>
+		</a>
+
+		<a class="btn"  href="#">
+
+			<i class="fab fa-github"></i>
+		</a>	
+	</div>
+
+</body>
+</html>
